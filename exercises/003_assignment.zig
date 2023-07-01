9dcbde10de2003ab7eff9031634baf0b4f052e3f
Arda Hamamcıoğlu <ardahamamcioglu@gmail.com>
2023-07-02 00:18:32 +0300

    Completed more exercises.

--- exercises/003_assignment.zig
+++ exercises/003_assignment.zig
@@ -12,7 +12,7 @@
 //     var bar: u8 = 20;
 //
 // Example: foo cannot be negative and can hold 0 to 255
-//          bar CAN be negative and can hold -128 to 127
+//          bar CAN be negative and can hold -128 to 12
 //
 //     const foo: u8 = 20;
 //     const bar: i8 = -20;
@@ -34,12 +34,12 @@
 const std = @import("std");
 
 pub fn main() void {
-    const n: u8 = 50;
+    var n: u8 = 50;
     n = n + 5;
 
-    const pi: u8 = 314159;
+    const pi: u32 = 314159;
 
-    const negative_eleven: u8 = -11;
+    const negative_eleven: i8 = -11;
 
     // There are no errors in the next line, just explanation:
     // Perhaps you noticed before that the print function takes two
