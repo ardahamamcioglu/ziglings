2450b9148253b71048efcfde0c3124d9e644820b
Arda Hamamcıoğlu <ardahamamcioglu@gmail.com>
2023-07-03 00:39:38 +0300

    Solved some more

--- exercises/035_enums.zig
+++ exercises/035_enums.zig
@@ -20,7 +20,11 @@
 const std = @import("std");
 
 // Please complete the enum!
-const Ops = enum { ??? };
+const Ops = enum {
+    inc,
+    dec,
+    pow,
+};
 
 pub fn main() void {
     const operations = [_]Ops{
@@ -46,6 +50,7 @@ pub fn main() void {
                 current_value *= current_value;
             },
             // No "else" needed! Why is that?
+            //Answer: Because we have an enum, and the compiler knows that, we have all the possible values.
         }
 
         std.debug.print("{} ", .{current_value});
